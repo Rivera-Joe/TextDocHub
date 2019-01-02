ee4d4fc9699e80b00d8735a0563bc09cf2421612
Rivera-Joe <36744600+Rivera-Joe@users.noreply.github.com>
2019-01-02 15:53:05 -0600

    linked project to github repo

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TextDocHub
 Allows user to name and write to a text document. Also allows them to designate a filepath if wanted.
+
+Created by Joe Rivera on 01/02/2019
---
+++ Text Doc Hub.cpp
@@ -0,0 +1,55 @@
+// Text Doc Hub.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+//Created by Joe Rivera on 01/02/2019
+
+#include "pch.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "filing.h"
+
+using namespace std;
+
+int main()
+{
+	file newfile;
+	cout << "what would you like to name your file?\n\n";
+
+	newfile.getName();//runs function to set a name for the file
+
+	cout << "please enter the file data\n\n";
+
+	newfile.getData(); //runs a function to set the data to input into the file
+
+
+
+
+	cout << "would you like to designate a filepath? (y/n)\n\n";
+
+	char reply;
+	cin >> reply;
+	if (reply == 'y') {
+		cout << "enter the filepath\n\n";
+
+		newfile.getPath(); // function to set a path for the file
+
+
+
+		newfile.WPmakeFile(); // function to create file with filepath
+	}
+	else {
+		newfile.NPmakeFile(); // function to create file without a path
+	}
+	
+
+		
+
+		
+	
+
+
+	return 0;
+}
+
+
